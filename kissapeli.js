9069c315af0c5e8aa79691c0eb086c7070cdade0
olli-hick <olliernest@gmail.com>
2021-02-04 17:02:54 +0200

    projekti aloitettu

---
+++ kissapeli.js
@@ -0,0 +1,10 @@
+let tasutakuva;
+let kissakuva;
+
+let taustan_korkeus = 400;
+let taustan_leveys = 800;
+
+function preload() {
+  taustakuva = loadImage("https://igno.cc/opetus/kuvat/tausta.png");
+  kissakuva = loadImage("https://igno.cc/opetus/kuvat/cat.png");
+}
